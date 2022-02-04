d7aa558489ca696ccb87042655638f7b13139914
Andy Soloman <andyjsoloman@gmail.com>
2022-02-03 21:17:43 -0800

    start styling palettelist

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,15 +2,51 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import seedColors from './SeedColors';
+import styled from 'styled-components';
+
+const RootDiv = styled.div`
+background-color: palevioletred;
+height: 100%;
+display: flex;
+align-items: flex-start;
+justify-content: center;`;
+
+const ContainerDiv = styled.div`
+width: 50%;
+display: flex;
+align-items: flex-start;
+flex-direction: column;
+flex-wrap: wrap;
+`;
+
+const StyledNav = styled.nav`
+display: flex;
+width: 100%;
+justify-content: space-between;
+color:white;
+`;
+
+const PalettesDiv = styled.div`
+box-sizing: border-box;
+width:100%;
+display: grid;
+grid-template-columns: repeat(3,30%);
+grid-gap: 5%;
+`;
 
 function PaletteList() {
 
-    return <div>
-        <h1>THIS IS PALETTE LIST</h1>
-
-        {seedColors.map(palette => (
-            <MiniPalette {...palette} />))}
-    </div>;
+    return <RootDiv>
+        <ContainerDiv>
+            <StyledNav>
+                <h1>React Color Palettes</h1>
+            </StyledNav>
+            <PalettesDiv>
+                {seedColors.map(palette => (
+                    <MiniPalette {...palette} />))}
+            </PalettesDiv>
+        </ContainerDiv>
+    </RootDiv>;
 }
 
 export default PaletteList;
